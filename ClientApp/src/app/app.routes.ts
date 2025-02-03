dc71449727d2690e4483d062c6f7f0526d93c42b
Andrey <iliev.andreyy@gmail.com>
2025-02-03 22:37:53 +0200

    Added httpProvider and configured page routes

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../identity/login/login.component';
+import { ServerTemperatureComponent } from '../serverTemperature/server-temperature/server-temperature.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'temperature', component: ServerTemperatureComponent },
+];
